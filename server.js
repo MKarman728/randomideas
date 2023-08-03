394c01267c8eaa0e137b683fe4275f9dc998eee9
Mark Karman <mkarman08@gmail.com>
2023-08-03 16:07:53 -0700

    added post put, and delete to API

--- server.js
+++ server.js
@@ -1,45 +1,17 @@
 const express = require('express');
 const port = 5001;
-const ideas = [
-    {
-        id: 1,
-        text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-        tag: 'Technology',
-        username: 'TonyStark',
-        date: '2022-01-02'
-    },
-    {
-        id: 2,
-        text: 'Milk cartons that turn a different color the older that your milk getting',
-        tag: 'Inventions',
-        username: 'SteveRogers',
-        date: '2022-01-02'
-    },
-    {
-        id: 3,
-        text: 'ATM location app which lets you know where the closest ATM is and if it s in service',
-        tag: 'Software',
-        username: 'BruceBanner',
-        date: '2022-01-02'
-    }
-]
+
 const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send({ message: 'Welcome to the RandomIdeas API' });
 });
 
-//Get all ideas
-app.get('/api/ideas', (req, res) => {
-    res.json({ success: true, data: ideas });
-});
-
-app.get('/api/ideas/:id', (req, res) => {
-    const idea = ideas.find((idea) => idea.id === +req.params.id);
-    if (!idea) {
-        return res.status(404).json({ success: false, error: 'Resource not found' });
-    }
-    res.json({ success: true, data: idea });
-});
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
 
 app.listen(port, () => { console.log(`Server listening on port ${port}`) });
