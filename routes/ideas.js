4b6a0fe7d01bc4b08f77d1ca875afb1069e1a319
Mark Karman <mkarman08@gmail.com>
2023-08-05 14:53:56 -0700

    added delete to API

--- routes/ideas.js
+++ routes/ideas.js
@@ -2,31 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Idea = require('../models/Idea');
 
-const ideas = [
-    {
-        id: 1,
-        text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-        tag: 'Technology',
-        username: 'TonyStark',
-        date: '2022-01-02'
-    },
-    {
-        id: 2,
-        text: 'Milk cartons that turn a different color the older that your milk getting',
-        tag: 'Inventions',
-        username: 'SteveRogers',
-        date: '2022-01-02'
-    },
-    {
-        id: 3,
-        text: 'ATM location app which lets you know where the closest ATM is and if it s in service',
-        tag: 'Software',
-        username: 'BruceBanner',
-        date: '2022-01-02'
-    }
-]
-
-
 //Get all ideas
 router.get('/', async (req, res) => {
     try {
@@ -87,13 +62,14 @@ router.put('/:id', async (req, res) => {
 });
 
 //Delete idea
-router.delete('/:id', (req, res) => {
-    const idea = ideas.find((idea) => idea.id === +req.params.id);
-    if (!idea) {
-        return res.status(404).json({ success: false, error: 'Resource not found' });
+router.delete('/:id', async (req, res) => {
+    try {
+        await Idea.findByIdAndDelete(req.params.id);
+        const ideas = await Idea.find()
+        res.json({ success: true, data: ideas });
+    } catch (error) {
+        res.status(500).json({success: false, error: "Something went wrong"});
     }
-    ideas.forEach((idea, idx) => idea.id === +req.params.id && ideas.splice(idx, 1));
 
-    res.json({ success: true, data: ideas });
 });
 module.exports = router;
